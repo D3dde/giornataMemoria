2ee9be3ba5142137ff9e6590537acdcc83d1082f
D3dde <daniel.lazza@tiscali.it>
2025-01-23 20:40:56 +0100

    point and click works

--- scripts/main.gd
+++ scripts/main.gd
@@ -1,6 +1,10 @@
 extends Node3D
 
+
 @export var camera : Camera3D
+@export var default_position : Marker3D
+var focussedBody : StaticBody3D
+
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -12,18 +16,24 @@ func _process(delta: float) -> void:
 	pass
 
 
-func _on_static_body_3d_input_event(camera: Node, event: InputEvent, event_position: Vector3, normal: Vector3, shape_idx: int) -> void:
-	if event is InputEventMouseButton and event.pressed:
-		if event.is_action_pressed("click"):
-			print("Collision shape clicked!")
-			var tween = get_tree().create_tween()
-			tween.tween_property(camera, "position", $items/immagine/Marker3D.position, 1)
-			$items/immagine/Label.show()
+func _on_immagine_collision_click(event : InputEvent, node : Node) -> void:
+	if event.is_action_pressed("click"):
+		print("Collision shape clicked!")
+		focussedBody = node
+		
+		var tween = get_tree().create_tween()
+		tween.tween_property(camera, "position", node.get_node("Marker3D").global_position, 1).set_trans(Tween.TRANS_CUBIC)
+		tween.parallel().tween_property(camera, "rotation", node.get_node("Marker3D").global_rotation, 1).set_trans(Tween.TRANS_CUBIC)
+		focussedBody.get_node("Label").show()
 
 
 func _input(event: InputEvent) -> void:
-	if event is InputEventKey and event.pressed and event.is_action_pressed("esc"):
+	if event.is_action_pressed("esc"):
 		print("Returning to default position")
+		if (focussedBody != null):
+			focussedBody.get_node("Label").hide()
+			focussedBody = null
+			
 		var tween = get_tree().create_tween()
-		tween.tween_property(camera, "position", $DefaultPosition.position, 1)
-		$items/immagine/Label.hide()
+		tween.tween_property(camera, "position", default_position.global_position, 0.5).set_trans(Tween.TRANS_SINE)
+		tween.parallel().tween_property(camera, "rotation", default_position.global_rotation, 0.5).set_trans(Tween.TRANS_SINE)
